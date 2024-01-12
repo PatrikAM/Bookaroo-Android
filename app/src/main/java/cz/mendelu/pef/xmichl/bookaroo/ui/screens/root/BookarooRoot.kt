ca303732c742db4f936dd6a3cb8451b9bde19906
Patrik Michl <xmichl@mendelu.cz>
2024-01-12 18:59:33 +0100

    splash,launcher-icon

--- app/src/main/java/cz/mendelu/pef/xmichl/bookaroo/ui/screens/root/BookarooRoot.kt
+++ app/src/main/java/cz/mendelu/pef/xmichl/bookaroo/ui/screens/root/BookarooRoot.kt
@@ -1,14 +1,18 @@
 package cz.mendelu.pef.xmichl.bookaroo.ui.screens.root
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import cz.mendelu.pef.xmichl.bookaroo.R
 import cz.mendelu.pef.xmichl.bookaroo.model.UiState
+import cz.mendelu.pef.xmichl.bookaroo.ui.elements.BaseScreen
 import cz.mendelu.pef.xmichl.bookaroo.ui.screens.destinations.ListOfLibrariesScreenDestination
 import cz.mendelu.pef.xmichl.bookaroo.ui.screens.destinations.LoginScreenDestination
 import cz.mendelu.pef.xmichl.bookaroo.ui.screens.login.LoginErrors
@@ -41,12 +45,12 @@ fun BookarooRoot(
         } else {
             navigator.navigate(LoginScreenDestination())
         }
+    } else {
+//        BaseScreen(topBarText = "", isNavScreen = false) {
+//            Image(
+//                painterResource(id = R.drawable.bookaroo),
+//                contentDescription = null
+//            )
+//        }
     }
-
-    // TODO: check for user logged in
-    // TODO: show login screen
-//    navigator.navigate(LoginScreenDestination())
-    // TODO: download all necessary data, show splash with loading
-
-//    navigator.navigate(ListOfLibrariesScreenDestination())
 }
