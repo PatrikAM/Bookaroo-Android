a06b0336f68730b8ea326dbee5bcbd5fe3690307
Patrik Michl <xmichl@mendelu.cz>
2024-01-14 19:32:42 +0100

    test-impl

--- app/src/androidTest/java/cz/mendelu/pef/xmichl/bookaroo/fakeRepostiory/LibraryFakeRepository.kt
+++ app/src/androidTest/java/cz/mendelu/pef/xmichl/bookaroo/fakeRepostiory/LibraryFakeRepository.kt
@@ -1,6 +1,7 @@
 package cz.mendelu.pef.xmichl.bookaroo.fakeRepostiory
 
 import cz.mendelu.pef.xmichl.bookaroo.architecture.CommunicationResult
+import cz.mendelu.pef.xmichl.bookaroo.architecture.Error
 import cz.mendelu.pef.xmichl.bookaroo.communication.library.ILibraryRemoteRepository
 import cz.mendelu.pef.xmichl.bookaroo.mock.BookarooLibrariesServerMock
 import cz.mendelu.pef.xmichl.bookaroo.model.Library
@@ -14,6 +15,21 @@ class LibraryFakeRepository @Inject constructor(): ILibraryRemoteRepository {
     }
 
     override suspend fun createLibrary(library: String): CommunicationResult<Library> {
-        TODO("Not yet implemented")
+        return CommunicationResult.Success(
+            Library("newlyCreated", name = library, ownerId = null)
+        )
+    }
+
+    override suspend fun fetchLibrary(libraryId: String): CommunicationResult<Library> {
+        return if (BookarooLibrariesServerMock
+            .all
+            .any { bookShop -> bookShop.id == libraryId }
+            ) {
+            return CommunicationResult.Success(BookarooLibrariesServerMock
+                .all
+                .first() { bookShop -> bookShop.id == libraryId })
+        } else {
+            return CommunicationResult.Error(error = Error(code = 404, message = null))
+        }
     }
 }
